ecef46601f5f67dcfad8d08f906ad5e6368207e5
Thuhaa <josephthuha@gmail.com>
2021-02-27 17:56:03 +0300

    Overlaid the World Polygon over the Leaflet Basemap

--- countries/views.py
+++ countries/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from .models import WorldCountries
+from django.core.serializers import serialize
+import json
+
+
 
 def homepage_view(request):
-	world_countries = WorldCountries.objects.all()
+	countries = serialize('geojson', WorldCountries.objects.all(),
+          geometry_field='geom',
+          fields=('cntry_name','objectid'))
+	countries_json = json.loads(countries)
+	print(type(countries_json))
+	return render(request, 'countries/homepage.html', {'countries_json':countries_json})
 
-	return render(request, 'countries/homepage.html')
-# Create your views here.
