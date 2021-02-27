b222ce54de17351abd39c53e210afc19d707e9aa
Thuhaa <josephthuha@gmail.com>
2021-02-27 23:02:17 +0300

    Updated the comments on the code

--- countries/models.py
+++ countries/models.py
@@ -1,13 +1,20 @@
 #from django.db import models
+
+# This model was generated automatically with the python manage.py ogrinspect command
+# Note that the models module has been imported from the django.contrib.gis.db
 from django.contrib.gis.db import models
 class WorldCountries(models.Model):
     objectid = models.IntegerField()
     cntry_name = models.CharField(max_length=39)
     geom = models.MultiPolygonField()
 
-
     def __str__(self):
+    	'''
+    	This function ensures that the name of the class instances are referenced by their names(cntry_names)
+    	'''
     	return self.cntry_name
 
+
+    # This class ensures that the right plural(in this case) is used
     class Meta:
     	verbose_name_plural = 'World Countries'
--- countries/views.py
+++ countries/views.py
@@ -6,9 +6,18 @@ import json
 
 
 def homepage_view(request):
+	'''
+	This is the homepage view(for now). This view will be grabbing countries data, serializing it, and
+	passing it as a dict to the views in readiness for the conversion of the dict to json, including
+	rendering the homepage.html file
+	'''
+
+	# Grab the countries data and serialize
 	countries = serialize('geojson', WorldCountries.objects.all(),
           geometry_field='geom',
           fields=('cntry_name','objectid'))
+
+	# Convert the countries data to a python dict and pass it in the context variable
 	countries_json = json.loads(countries)
 	return render(request, 'countries/homepage.html', {'countries_json':countries_json})
 
